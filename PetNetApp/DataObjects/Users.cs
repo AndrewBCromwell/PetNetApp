eaf750f8e91c78ab5d12afe08cbc21fa6f3a7aa2
Hoang Chu <kamikiburning2002@gmail.com>
2023-02-03 10:27:11 -0600

    EmployeeManagement

---
+++ PetNetApp/DataObjects/Users.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    public class Users
+    {
+        public int UsersId { get; set; }
+        public string GenderId { get; set; }
+        public string PronoundId { get; set; }
+        public int ShelterId { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Address { get; set; }
+        public string AddressTwo { get; set; }
+        public string Zipcode { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool Active { get; set; }
+        public bool Suspended { get; set; }
+    }
+}
--- PetNetApp/LogicLayer/MasterManager.cs
+++ PetNetApp/LogicLayer/MasterManager.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LogicLayerInterfaces;
 
 namespace LogicLayer
 {
     public class MasterManager
     {
+        public IUsersManager UsersManager { get; set; }
+        public MasterManager()
+        {
+            UsersManager = new UsersManager();
+        }
     }
 }
