9a7ef8505269c9556d1f7ae4c2eae8d1139c2783
William Rients <williamrients@gmail.com>
2023-02-10 18:17:38 -0600

    Adding stored procedures for features

--- Database/PetNet.bat
+++ Database/PetNet.bat
@@ -19,6 +19,8 @@ sqlcmd -S localhost -E -i StoredProceduresScripts\AddProcedure_SP.sql
 
 sqlcmd -S localhost -E -i StoredProceduresScripts\EmployeeManagement_SP.sql
 sqlcmd -S localhost -E -i StoredProceduresScripts\AnimalPostUpdate_SP.sql
+sqlcmd -S localhost -E -i StoredProceduresScripts\MedicalProfile_SP.sql
+sqlcmd -S localhost -E -i StoredProceduresScripts\AddAnimal_to_kennel_SP.sql
 
 ECHO .
 ECHO if no errors appear DB was created
---
+++ Database/StoredProceduresScripts/AddAnimal_to_kennel_SP.sql
@@ -0,0 +1,45 @@
+/***************************************************************
+William Rients
+Created: 2023/02/10
+Description:
+File containing stored procedure for adding an animal to a kennel
+and viewing animals with no assigned kennel
+****************************************************************/
+
+USE [PetNet_db_am]
+GO
+
+/* Insert Animal into Kennel */
+/* William Rients */
+print '' print '*** creating sp_insert_animal_into_kennel_by_animalId_and_kennelId'
+GO
+CREATE PROCEDURE [dbo].[sp_insert_animal_into_kennel_by_animalId_and_kennelId]
+(
+	@KennelId		[int],
+	@AnimalId		[int]
+)
+AS
+	BEGIN
+		INSERT INTO	[dbo].[AnimalKenneling]
+			([KennelId], [AnimalId])
+		VALUES	
+			(@KennelId, @AnimalId)
+	END
+GO
+
+
+
+/* Select All Animals */
+/* William Rients */
+print '' print '*** creating sp_select_all_animals_without_kennel'
+GO
+CREATE PROCEDURE [dbo].[sp_select_all_animals_without_kennel]
+AS
+	BEGIN
+		SELECT	[Animal].[AnimalId], [AnimalName], [AnimalTypeId], [AnimalBreedId], [Personality], [RecievedDate], [Description],
+				[MicrochipSerialNumber], [Aggressive], [AggressiveDescription], [ChildFriendly], [NeuterStatus], [Notes], [AnimalStatusId]
+		FROM	[Animal] LEFT JOIN [AnimalKenneling] 
+				ON [Animal].[AnimalId] = [AnimalKenneling].[AnimalId]
+		WHERE 	[AnimalKenneling].[AnimalId] IS NULL
+	END
+GO
