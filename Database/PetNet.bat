9332dc416ef4f786c062a492314639e887f17668
Gwen Arman <gwen-arman@student.kirkwood.edu>
2023-02-16 16:19:16 -0600

    Changing colors for KennelUserControl

--- Database/PetNet.bat
+++ Database/PetNet.bat
@@ -32,6 +32,7 @@ sqlcmd -S localhost -E -i StoredProceduresScripts\ViewTreatment_SP.sql
 sqlcmd -S localhost -E -i StoredProceduresScripts\ViewKennel_SP.sql
 sqlcmd -S localhost -E -i StoredProceduresScripts\RemoveKennels_SP.sql
 sqlcmd -S localhost -E -i StoredProceduresScripts\AddKennel_SP.sql
+
 ECHO .
 ECHO if no errors appear DB was created
 PAUSE
