2b9534db43e1054fb5e6336b924c67ce058e85d2
Asa Armstrong <asaarmstrong12@gmail.com>
2023-02-16 19:31:49 -0600

    KenOccupancyUpadte

--- Database/PetNet.bat
+++ Database/PetNet.bat
@@ -33,9 +33,7 @@ sqlcmd -S localhost -E -i StoredProceduresScripts\ViewKennel_SP.sql
 sqlcmd -S localhost -E -i StoredProceduresScripts\RemoveKennels_SP.sql
 sqlcmd -S localhost -E -i StoredProceduresScripts\AddKennel_SP.sql
 sqlcmd -S localhost -E -i StoredProceduresScripts\AnimalDOD513-514_SP.sql
-
-
-sqlcmd -S localhost -E -i KenOccupancyUpdate-333_SP.sql
+sqlcmd -S localhost -E -i StoredProceduresScripts\KenOccupancyUpdate-333_SP.sql
 
 ECHO .
 ECHO if no errors appear DB was created
