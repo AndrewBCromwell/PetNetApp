06021869678d02ce9d623468418b0f4dd8bc654f
Barry Mikulas <salukim@gmail.com>
2023-02-20 22:20:28 -0600

    database script error fix

--- Database/SampleDataScripts/RoleManagement_SD.sql
+++ Database/SampleDataScripts/RoleManagement_SD.sql
@@ -31,33 +31,5 @@ INSERT INTO [dbo].[Users]
 		[Phone]
 		)
     VALUES
-		("Male", "N/A", "999999", "Barry", "Mikulas", "bmikulas@company.com", "2 Kirkwood Parkway", "Apt 4", '50001', "3198675309"),
-		("Male", "He/Him", "888888", "Stephen", "Jaurigue", "stephen@company.com", "123 Kirkwood Parkway", "Apt 210", "50001", "3195555555"),
-		("Female", "She/Her", "777777", "Molly", "Meister", "molly@company.com", "456 Kirkwood Parkway", "Apt 256", "50001", "3196666666"),
-		('Male', 'He/Him', '666666', 'Tyler', 'Hand', 'tyler@company.com', '789 Kirkwood Parkway', 'Apt 240', '50002', '3197777777')
-GO
-
-print '' print '*** Inserting Role Records'
-GO
-INSERT INTO [dbo].[Role]
-		(
-		[RoleId],
-		[Description]
-		)
-	VALUES
-		('Volunteer',''),
-		('Admin',''),
-		('Vet',''),
-		('Manager',''),
-		('Employee','')
-GO
-
-print '' print '*** Inserting UserRoles Data'
-GO
-INSERT INTO [dbo].[UserRoles]
-		([RoleId], [UsersId])
-	VALUES
-		('Admin','100001'),
-		('Employee','100001'),
-		('Volunteer','100000')
+		("Male", "He/Him", "100000", "Barry", "Mikulas", "bmikulas@company.com", "2 Kirkwood Parkway", "Apt 4", '50001', "3198675309")
 GO
