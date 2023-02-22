02f8fc8cc75c4cd2bed17016b5be0d73174b2e75
Asa Armstrong <asaarmstrong12@gmail.com>
2023-02-22 16:38:05 -0600

    KenOccupancyUpdate

--- Database/PetNet.bat
+++ Database/PetNet.bat
@@ -40,6 +40,7 @@ sqlcmd -S localhost -E -i StoredProceduresScripts\EditProcedure_SP.sql
 sqlcmd -S localhost -E -i StoredProceduresScripts\KenOccupancyUpdate-333_SP.sql
 sqlcmd -S localhost -E -i StoredProceduresScripts\RemoveRole-006_SP.sql
 
+
 ECHO .
 ECHO if no errors appear DB was created
 PAUSE
