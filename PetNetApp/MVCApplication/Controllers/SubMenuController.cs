380114e18955fd633fc6e347106217076f13a881
mads rhea <k0749548@student.kirkwood.edu>
2023-03-06 10:55:50 -0600

    petnet shell - will be updating throughout the day

---
+++ PetNetApp/MVCApplication/Controllers/SubMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class SubMenuController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Animals()
+        {
+            return View();
+        }
+
+        public ActionResult Shelters()
+        {
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            return View();
+        }
+
+        public ActionResult Community()
+        {
+            return View();
+        }
+
+        public ActionResult Management()
+        {
+            return View();
+        }
+
+        public ActionResult Fundraising()
+        {
+            return View();
+        }
+    }
+}
