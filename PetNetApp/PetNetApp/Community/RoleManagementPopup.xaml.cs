3184ce3c45b3c7587749372d20c5dfd4b9f752e0
Barry Mikulas <salukim@gmail.com>
2023-03-09 07:55:49 -0600

    updated to load center owner - add rolemgmt to non dev folder

--- PetNetApp/PetNetApp/Community/RoleManagementPopup.xaml.cs
+++ PetNetApp/PetNetApp/Community/RoleManagementPopup.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace WpfPresentation.Community
+namespace WpfPresentation.Development.Community
 {
     /// <summary>
     /// Interaction logic for RoleManagementPopup.xaml
