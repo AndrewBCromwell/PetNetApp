7fd87e4d2e5607ac3450f0ade0f41f825c79b280
Gwen Arman <gwen-arman@student.kirkwood.edu>
2023-03-26 17:23:20 -0500

    Adding interface comments

--- PetNetApp/DataAccessLayerInterfaces/IReplyAccessor.cs
+++ PetNetApp/DataAccessLayerInterfaces/IReplyAccessor.cs
@@ -9,9 +9,37 @@ namespace DataAccessLayerInterfaces
 {
     public interface IReplyAccessor
     {
+        /// <summary>
+        /// Author: Gwen Arman
+        /// Date: 2023-03-26
+        /// Description: Selects all replies that an admin or moderator could see
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
         List<ReplyVM> SelectAllRepliesByPostId(int postId);
+        /// <summary>
+        /// Author: Gwen Arman
+        /// Date: 2023-03-26
+        /// Description: Selects active replies
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
         List<ReplyVM> SelectActiveRepliesByPostId(int postId);
+        /// <summary>
+        /// Author: Gwen Arman
+        /// Date: 2023-03-26
+        /// Description: Selects a count of replies for an admin or moderator
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
         int SelectCountRepliesByPostId(int postId);
+        /// <summary>
+        /// Author: Gwen Arman
+        /// Date: 2023-03-26
+        /// Description: Selects active replies
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
         int SelectCountActiveRepliesByPostId(int postId);
     }
 }
--- PetNetApp/LogicLayerInterfaces/IReplyManager.cs
+++ PetNetApp/LogicLayerInterfaces/IReplyManager.cs
@@ -9,9 +9,37 @@ namespace LogicLayerInterfaces
 {
     public interface IReplyManager
     {
+        /// <summary>
+        /// Author: Gwen Arman
+        /// Date: 2023-03-26
+        /// Description: Retrieves all replies that an admin or moderator could see by post id
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
         List<ReplyVM> RetrieveAllRepliesByPostId(int postId);
+        /// <summary>
+        /// Author: Gwen Arman
+        /// Date: 2023-03-26
+        /// Description: Retrieves active replies 
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
         List<ReplyVM> RetrieveActiveRepliesByPostId(int postId);
+        /// <summary>
+        /// Author: Gwen Arman
+        /// Date: 2023-03-26
+        /// Description: Retrieves count of replies for an admin or moderator
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
         int RetrieveCountRepliesByPostId(int postId);
+        /// <summary>
+        /// Author: Gwen Arman
+        /// Date: 2023-03-26
+        /// Description: Retrieves count of active replies
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
         int RetrieveCountActiveRepliesByPostId(int postId);
     }
 }
