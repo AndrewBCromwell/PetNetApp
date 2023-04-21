288c2a2db13525675c69d4aaf675c3066dcfc886
Barry Mikulas <salukim@gmail.com>
2023-04-21 11:55:52 -0500

    View FundraisingEvents MVC

--- Database/SampleDataScripts/PetNet_db_sample_data.sql
+++ Database/SampleDataScripts/PetNet_db_sample_data.sql
@@ -789,9 +789,9 @@ INSERT INTO [dbo].[FundraisingEvent]
         [AdditionalInfo]
 		)
 	VALUES
-		(100001, 100000,100000, 'Shelter in Need', '2023-07-12', '2023-07-14', 'You got a shelter in need', 'It will be fun!'),
-        (100001, 100000,100000, 'Puppy Fun Day', '2023-07-16', '2023-07-18', 'Watch cute puppies play', 'It will be fun!'),
-        (100001, 100000,100000, 'Give me your money', '2023-07-20', '2023-07-21', 'I want money', 'It will be fun!')
+		(100001, 100000,100000, 'Shelter in Need', '2023-07-12 10:00:00 AM', '2023-07-12 10:00:00 PM', 'You got a shelter in need', 'It will be fun!'),
+        (100001, 100000,100000, 'Puppy Fun Day', '2023-07-16 10:30:00 AM', '2023-07-16 10:00:00 PM', 'Watch cute puppies play', 'It will be fun!'),
+        (100001, 100000,100000, 'Give me your money', '2023-07-20 11:00:00 AM', '2023-07-20  05:00:00 PM', 'I want money', 'It will be fun!')
 GO
 
 print '' print '*** Creating Donation sample data'
